64c2e763c5fe817520ae358a519cbf4975aca4c5
yongma16 <17685043634@163.com>
2020-12-11 23:12:53 +0800

    初始化工程

---
+++ src/test/java/com/yma16/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yma16.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
